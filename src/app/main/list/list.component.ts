188938ec615464ba215d53b3a088011ad9b7f5ac
lzane <cn.zanelee@gmail.com>
2016-11-30 11:57:47 +0800

    add dropdown select

--- src/app/main/list/list.component.ts
+++ src/app/main/list/list.component.ts
@@ -1,16 +1,20 @@
 import {Component, OnInit} from "@angular/core";
 import {ReminderService} from "../../_services/reminder.service";
 import {Reminder} from "../../_model/reminder.model";
+import {ChannelService} from "../../_services/channel.service";
+
+declare var $: any;
 
 @Component({
-    templateUrl: 'list.component.html'
+    templateUrl: 'list.component.html',
+    styleUrls:['list.component.css']
 })
 
 export class ListComponent implements OnInit {
 
     reminders: Reminder[];
-
-    date;
+    newReminder: Reminder = new Reminder();
+    myOwnChannels = [];
 
     datepickerOpts = {
         autoclose: true,
@@ -22,21 +26,35 @@ export class ListComponent implements OnInit {
 
 
     ngOnInit(): void {
-        this.date = new Date();
+        //get all reminder
         this.reminderService.getAllReminder().subscribe(
             res => {
                 this.reminders = res.reminders;
+                $('.selectpicker').selectpicker('render');
+                $('.selectpicker').on('show.bs.select', function () {
+                    $(this).selectpicker('refresh');
+                });
+
+                $('.selectpicker').on('changed.bs.select', function (e) {
+                    var selectedChannel = $(e.currentTarget).val();
+                    console.log(selectedChannel);
+                });
             },
             err => console.log(err)
         );
+        //get all my channels
+        this.channelService.getMyOwnChannels().subscribe(
+            res=>{
+                this.myOwnChannels = res;
+            },
+            err => console.log(err)
+        )
+
     }
 
 
-    constructor(private reminderService: ReminderService) {
-    }
-
-    click(){
-        console.log(this.date.getTime());
+    constructor(private reminderService: ReminderService,
+                private channelService: ChannelService) {
     }
 
 }
